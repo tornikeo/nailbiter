e68835e6534630656f1d8ceecc14c525379b16d2
tornikeo <tonop15@freeuni.edu.ge>
2020-07-06 20:26:07 +0400

    if allowed hosts is not set, make warning and go with default

--- app/nailbiter/settings.py
+++ app/nailbiter/settings.py
@@ -25,8 +25,11 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", default=True)
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
-
+try:
+    ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+except AttributeError as e:
+    print("Failed to get DJANGO_ALLOWED_HOSTS, using default localhost")
+    ALLOWED_HOSTS = 'localhost'
 
 # Application definition
 
