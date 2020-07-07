3a30264d759c51fba70c98c4853d333f31edc38f
tornikeo <tonop15@freeuni.edu.ge>
2020-07-07 16:17:45 +0400

    add FTs for classifier api

--- app/functional_tests/test_classifier_api.py
+++ app/functional_tests/test_classifier_api.py
@@ -1,5 +1,34 @@
-from django.test import LiveServerTestCase
-from django.test.client import 
+from functional_tests.base import FunctionalTestCase, Utils
+import os
+import glob
+import json
+import numpy as np
 
-class ClassifierTests(LiveServerTestCase):
-    def test_api_call_succeeds(self):
+class ClassifierTests(FunctionalTestCase):
+    def test_user_can_get_image_classification(self):
+        #User lists his/her files
+        this_dir = os.path.dirname(os.path.abspath(__file__))
+        image_dir = os.path.join(this_dir, 'sample_images')
+        positive_img_dir = os.path.join(image_dir, 'positive')
+        negative_img_dir = os.path.join(image_dir, 'negative')
+        positive_images = glob.glob(os.path.join(positive_img_dir,'*.jpg'))
+        negative_images = glob.glob(os.path.join(negative_img_dir,'*.jpg'))
+
+        #User creates a json request with first positive image
+        image = Utils.read_img_to_array(positive_images[0])
+        
+        # User sends the request
+        request = {
+            'image': image.tolist()
+        }
+        response = self.client.get('/image_classifier', 
+            data = request
+        ).json()
+
+        # Status must be OK
+        self.assertEquals(response['status'].lower(), 'ok')
+
+        # Prediction must be an array
+        self.assertIsInstance(response['prediction'], list)
+
+
