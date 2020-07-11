dbfec4ed02edc5709c79df8b105bfe031270998f
tornikeo <tonop15@freeuni.edu.ge>
2020-07-11 12:39:15 +0400

    need requests module to use tf-serving container

--- app/image_classifier/views.py
+++ app/image_classifier/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
+from .model.serving import predict
+import json
 # Create your views here.
+# Request is of format 
+# {
+#  "image":[[]]
+# }
+
+SERVER_URL = 'http://localhost:8501/v1/models/model:predict'
+
 def get_image_classification(request):
-    return JsonResponse({
-        'prediction':[.5, .5],
-        'status':'ok'
-    })
+    prediction = predict([
+        json.loads(request.GET['instances'])
+    ])
+
+    return JsonResponse(prediction)
